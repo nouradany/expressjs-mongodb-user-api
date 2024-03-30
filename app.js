040e7e5709a1bf269bdc084e8e34f0f856c72ebc
lamia <nouradany3@gmail.com>
2024-03-30 10:42:28 +0000

    create API EndpointD(create new user)

--- app.js
+++ app.js
@@ -19,4 +19,17 @@ app.get('/users/:id', getUser, (req, res) => {
     res.json(res.user);
     });
 
-
+// Create a new user
+app.post('/users', async (req, res) => {
+    const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body. password
+});   
+    try {
+    const newUser = await user.save();
+    res.status(201). json(newUser);
+    } catch (err) {
+    res.status(400). json({ message: err.message });
+}
+});
