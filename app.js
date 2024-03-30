61a3b196c64abcb7a0c5692af3f6e37b5cf5fc1c
lamia <nouradany3@gmail.com>
2024-03-30 10:36:48 +0000

    create API EndpointD(get all users)

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const User = require('./user');
 const app = express();
 app.use(express.json());
 
+// Get all users
+app.get('/users', async (req, res) => {
+    try {
+    const users = await User.find();
+    res. json(users);
+    } catch (err) {
+    res.status(500). json({ message: err.message });
+}
+});
